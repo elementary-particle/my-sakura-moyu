0d88421ca65a06af140ed034cb84844b156d3a5a
Kuchinashi <mio@ahympia.com>
2022-09-20 12:00:52 +0800

    Add MDUI as User Interface

--- tcclient/vue.config.js
+++ tcclient/vue.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    client: {
+      webSocketURL: 'ws://0.0.0.0:3000/ws',
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  },
   chainWebpack: config => {
     config.plugin("html").tap(
       args => {
